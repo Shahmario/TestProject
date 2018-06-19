ddf68684dc835353367589a1fdc1e0359c7ecd7a
Shahmario <40308314+Shahmario@users.noreply.github.com>
2018-06-18 22:27:26 -0500

    Added some scripts to test out the capabilites of source tree. Wound up impressed and will continue to use during game production

---
+++ Assets/Hola.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hola : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("The meaning of life is 42.....or is it?");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
