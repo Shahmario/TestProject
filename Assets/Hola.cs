81c17577145ed52e2d92914dd40b24c466d8ee80
Shahmario <40308314+Shahmario@users.noreply.github.com>
2018-06-18 22:29:45 -0500

    Saved project and scene and added a bit to the script

--- Assets/Hola.cs
+++ Assets/Hola.cs
@@ -7,6 +7,7 @@ public class Hola : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         print("The meaning of life is 42.....or is it?");
+        print("I'm pretty sure the meaning of life can be sausages as well...");
 	}
 	
 	// Update is called once per frame
